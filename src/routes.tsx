bbd56d8f3f43909001fd3710e7ec4645cba8a811
Gustavo Henrique <gustavohsdp@gmail.com>
2020-10-16 12:10:25 -0300

    Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,6 +8,9 @@ const { Navigator, Screen } = createStackNavigator();
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
 
+import SelectMapPosition from './pages/createOrphanage/SelectMapPosition';
+import OrphanageData from './pages/createOrphanage/OrphanageData';
+
 export default function Routes() {
   return (
     <NavigationContainer>
@@ -15,6 +18,10 @@ export default function Routes() {
         <Screen name="OrphanagesMap" component={OrphanagesMap}/>
 
         <Screen name="OrphanageDetails" component={OrphanageDetails}/>
+
+        <Screen name="SelectMapPosition" component={SelectMapPosition}/>
+
+        <Screen name="OrphanageData" component={OrphanageData}/>
       </Navigator>
     </NavigationContainer>
   )
