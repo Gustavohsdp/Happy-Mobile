a08a0ef680a0f2cffce93a40547f09b78f0a6e11
Gustavo Henrique <gustavohsdp@gmail.com>
2020-10-17 12:44:11 -0300

    Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,18 +10,43 @@ import OrphanageDetails from './pages/OrphanageDetails';
 
 import SelectMapPosition from './pages/createOrphanage/SelectMapPosition';
 import OrphanageData from './pages/createOrphanage/OrphanageData';
+import Header from './components/Header';
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{headerShown: false}}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap}/>
+      <Navigator screenOptions={{headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'}}}>
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanagesMap}
+        />
 
-        <Screen name="OrphanageDetails" component={OrphanageDetails}/>
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => < Header showCancel={false} title= 'Orfanato'/>
+          }}
+        />
 
-        <Screen name="SelectMapPosition" component={SelectMapPosition}/>
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => < Header title= 'Selecione no mapa'/>
+          }}
+        />
 
-        <Screen name="OrphanageData" component={OrphanageData}/>
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => < Header title= 'Informe os dados'/>
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   )
